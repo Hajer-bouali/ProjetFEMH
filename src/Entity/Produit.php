33b8c0296648c5ff060401a715d282131bc1bcb4
Malak-hadj-messaoud <59702360+malak-haj@users.noreply.github.com>
2022-04-07 20:57:53 +0100

    correction even et adher

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -46,6 +48,16 @@ class Produit
      * @ORM\ManyToOne(targetEntity=Stock::class, inversedBy="produit")
      */
     private $stock;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Evenement::class, mappedBy="produit")
+     */
+    private $evenements;
+
+    public function __construct()
+    {
+        $this->evenements = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -120,6 +132,33 @@ class Produit
     public function setStock(?Stock $stock): self
     {
         $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenements(): Collection
+    {
+        return $this->evenements;
+    }
+
+    public function addEvenement(Evenement $evenement): self
+    {
+        if (!$this->evenements->contains($evenement)) {
+            $this->evenements[] = $evenement;
+            $evenement->addProduit($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): self
+    {
+        if ($this->evenements->removeElement($evenement)) {
+            $evenement->removeProduit($this);
+        }
 
         return $this;
     }
